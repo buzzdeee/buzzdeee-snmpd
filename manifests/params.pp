9d2094ceb6394c5b49d625d4f62046aa169bec22
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2014-12-22 19:32:28 +0100

    Add .travis.yml, and update params.pp to only run for OpenBSD.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,19 @@
 class snmpd::params {
-  $listen_addr = '127.0.0.1'
-  $system_contact = "Charly Root (root@${domain})"
-  $system_description = 'Powered by OpenBSD'
-  $system_location = 'In the datacenter'
-  $seclevel = 'none'
-  $ro_community = 'public'
-  $rw_community = 'private'
-  $rw_disabled = true
-  $service_ensure = 'running'
-  $service_enable = true
+  case $::operatingsystem {
+    'OpenBSD': {
+                 $listen_addr = '127.0.0.1'
+                 $system_contact = "Charly Root (root@${domain})"
+                 $system_description = 'Powered by OpenBSD'
+                 $system_location = 'In the datacenter'
+                 $seclevel = 'none'
+                 $ro_community = 'public'
+                 $rw_community = 'private'
+                 $rw_disabled = true
+                 $service_ensure = 'running'
+                 $service_enable = true
+               }
+    default: {
+               fail("Your Operatingsystem $::operatingsystem is not supported")
+             }
+  }
 }
